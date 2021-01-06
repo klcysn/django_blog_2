35d54c6e7a01f3e55ff40401b1aaade2aa32b7fa
klcysn <klcysn@users.noreply.github.com>
2021-01-06 09:31:57 +0100

    Add utils and uuid

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import post_list, post_create
+from .views import post_list, post_create, post_detail
 
 app_name = "blog"
 urlpatterns = [
     path('', post_list, name="post-list" ),
     path('create/', post_create, name="create" ),
+    path('<slug:slug>/', post_detail, name="detail" ),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
 # Create your views here.
 
 def post_list(request):
-    qs = Post.objects.all()
+    qs = Post.objects.filter(status="published")
     context = {
         "object_list" : qs
     }
@@ -25,3 +25,11 @@ def post_create(request):
     }
     
     return render(request, "blog/post_create.html", context)
+
+
+def post_detail(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_detail.html", context)
