1a741d54eece60044b52cfad697127ab032aecc3
klcysn <klcysn@users.noreply.github.com>
2021-01-07 21:49:42 +0100

    Add profile models and create users app

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import config
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "blog.apps.BlogConfig",
+    "users.apps.UsersConfig"
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "media_root"
+
