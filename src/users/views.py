bc8e26680dcda78a8d6b8b27d3e755fbf52a1e87
klcysn <klcysn@users.noreply.github.com>
2021-01-09 16:39:18 +0100

    Add register page

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your views here.
+
+def register(request):
+    form = RegistrationForm(request.POST or None)
+    
+    if form.is_valid():
+        form.save()
+        # return redirect("login")
+    context = {
+        "form": form
+    }
+    return render(request, "users/register.html")
+
+def profile(request):
+    user_form = UserUpdateForm(request.POST or None, instance=request.user)
