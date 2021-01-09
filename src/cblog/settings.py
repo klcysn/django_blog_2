5f6d74f9c911eb090b3a396787265b01cf36bd6f
klcysn <klcysn@users.noreply.github.com>
2021-01-09 19:12:14 +0100

    Add password reset pages

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -133,3 +133,9 @@ LOGIN_REDIRECT_URL = "blog:post-list"
 
 LOGIN_URL = "login"
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER=config("EMAIL_USER")
+EMAIL_HOST_PASSWORD=config("EMAIL_PASSWORD")
